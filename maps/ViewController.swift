c55e2cf4c1a47c21f793639487813d75fdef3ea4
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-11 10:25:25 -0600

    basic map

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 41.287434
+        let longitude: CLLocationDegrees = -96.0997737
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
